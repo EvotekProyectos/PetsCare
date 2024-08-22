1d866b2bc183d0f76fb55d9d3bb7236e72aeca55
ackerman507 <ackerman507@hotmail.com>
2024-08-22 12:38:54 -0600

    22/08/24 - Se actualiza orden reciente a antiguo de bitácora

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -22,7 +22,7 @@ class LogController extends Controller
     }
 
     public function list(){
-        $logs = Log::with('user')->get();
+        $logs = Log::with('user')->orderBy('created_at', 'desc')->get();
         return DataTables::of($logs) ->make(true);
     }
 
