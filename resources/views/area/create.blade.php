11553494eda74c1969144a89f51a43ca8b070951
ackerman507 <ackerman507@hotmail.com>
2024-08-26 13:14:39 -0600

    26/08/24 - Se corrige indentación

--- resources/views/area/create.blade.php
+++ resources/views/area/create.blade.php
@@ -15,17 +15,17 @@
                         <div class="d-flex justify-content-between align-items-center">
                             <h4 id="card_title" class="text-primary text-uppercase">Crear área</h4>
                         </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-12 col-lg-9">
-                                    <form method="POST" action="{{ route('areas.store') }}" role="form"
-                                        enctype="multipart/form-data">
-                                        @csrf
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-lg-9">
+                                <form method="POST" action="{{ route('areas.store') }}" role="form"
+                                    enctype="multipart/form-data">
+                                    @csrf
 
-                                        @include('area.form')
+                                    @include('area.form')
 
-                                    </form>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
